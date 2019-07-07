5c7cb6e685e143f670f68dcc6e115fe4c31b6660
onewayticket255 <15365182027@163.com>
2019-07-07 21:59:29 +0800

    format

--- UnblockNeteaseMusic/basic_https.js
+++ UnblockNeteaseMusic/basic_https.js
@@ -1,69 +1,69 @@
 const http = require('http');
-const https=require('https')
+const https = require('https')
 const url = require('url');
 const net = require('net');
-const fs= require('fs')
+const fs = require('fs')
 
 
 //FakeHttpProxy
 const ssl = {
-	key: fs.readFileSync('server.key'),
-	cert: fs.readFileSync('server.crt')
+     key: fs.readFileSync('server.key'),
+     cert: fs.readFileSync('server.crt')
 }
 
 let HttpProxy = new https.Server(ssl)
 let ProxyPort = 7000
 
-HttpProxy.listen(ProxyPort,()=>console.log(`HttpProxy Starts at ${ProxyPort}`))
-         .on('error', e => console.log(e))
-         .on('request', (req, res) => {
+HttpProxy.listen(ProxyPort, () => console.log(`HttpProxy Starts at ${ProxyPort}`))
+     .on('error', e => console.log(e))
+     .on('request', (req, res) => {
 
-              console.log(req.url)
+          console.log(req.url)
 
-              let options =  {
-                protocol: `https:`,
-                hostname: req.headers.host.split(':')[0],
-                method: req.method,
-                port: 443,
-                path: url.parse(req.url).path,
-                headers: req.headers
-              }
-                  
-              console.log("options: "+options)
+          let options = {
+               protocol: `https:`,
+               hostname: req.headers.host.split(':')[0],
+               method: req.method,
+               port: 443,
+               path: url.parse(req.url).path,
+               headers: req.headers
+          }
 
-              res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8'});
-              res.write(`<html><body>Fake Site: ${options.protocol}//${options.hostname}</body></html>`)
-              res.end();
-         })
+          console.log("options: " + options)
+
+          res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+          res.write(`<html><body>Fake Site: ${options.protocol}//${options.hostname}</body></html>`)
+          res.end();
+     })
 
 
 //HttpTunnel
 let HttpTunnel = new http.Server()
 let TunnelPort = 6789
 HttpTunnel.listen(TunnelPort, () => console.log(`HttpTunnel Starts at ${TunnelPort}`))
-          .on('error', e => console.log(e))
-          .on('connect', (request, client_socket, head) => {
-               let main_url = url.parse(`https://${request.url}`)
-               console.log(`CONNECT ${main_url.hostname}:${main_url.port}`)
-               client_socket.on('error',e=>console.log(e))
-              
+     .on('error', e => console.log(e))
+     .on('connect', (request, client_socket, head) => {
+          let main_url = url.parse(`https://${request.url}`)
+          console.log(`CONNECT ${main_url.hostname}:${main_url.port}`)
+          client_socket.on('error', e => console.log(e))
 
-            
-               let server_socket = net.connect(7000, `127.0.0.1`)          
-               server_socket.on('error',e=>console.log(e))
-                            .on('connect',() => {
-                                                               
-                                client_socket.write('HTTP/1.1 200 Connection Established\r\n\r\n')// first:\r\n CRLF second:\r\n new empty line between header and body
-                                //server_socket.write(head)
-  
-                                client_socket.pipe(server_socket)                             
-                                server_socket.pipe(client_socket)
 
-                                                                                                 
+
+          let server_socket = net.connect(7000, `127.0.0.1`)
+          server_socket.on('error', e => console.log(e))
+               .on('connect', () => {
+
+                    client_socket.write('HTTP/1.1 200 Connection Established\r\n\r\n')// first:\r\n CRLF second:\r\n new empty line between header and body
+                    //server_socket.write(head)
+
+                    client_socket.pipe(server_socket)
+                    server_socket.pipe(client_socket)
+
+
                })
-               
 
-               
-               
-          })
+
+
+
+     })
 
--- UnblockNeteaseMusic/netease_crypto.js
+++ UnblockNeteaseMusic/netease_crypto.js
@@ -1,31 +1,31 @@
-const crypto =require("crypto")
-const fs     = require('fs');
-const url    = require('url')
-const eapikey="e82ckenh8dichen8"
+const crypto = require("crypto")
+const fs = require('fs');
+const url = require('url')
+const eapikey = "e82ckenh8dichen8"
 
 
-function decrypt(cipher_text){
-    let decipher=crypto.createDecipheriv("aes-128-ecb",eapikey,null)
-    let plain_text=decipher.update(cipher_text,"hex","utf8")
-    plain_text+=decipher.final("utf8")
+function decrypt(cipher_text) {
+    let decipher = crypto.createDecipheriv("aes-128-ecb", eapikey, null)
+    let plain_text = decipher.update(cipher_text, "hex", "utf8")
+    plain_text += decipher.final("utf8")
     return plain_text
-} 
+}
 
-function encrypt(plain_text){
-    let cipher = crypto.createCipheriv('aes-128-ecb', eapikey, null)   
-    let cipher_text=cipher.update(plain_text,"utf8","hex")
-    cipher_text+=cipher.final("hex")
+function encrypt(plain_text) {
+    let cipher = crypto.createCipheriv('aes-128-ecb', eapikey, null)
+    let cipher_text = cipher.update(plain_text, "utf8", "hex")
+    cipher_text += cipher.final("hex")
     return cipher_text
 }
 
 
-function decrypt_params(params){
-   let text=decrypt(params)
-   let result=text.split("-36cd479b6b5-")
-   return result
+function decrypt_params(params) {
+    let text = decrypt(params)
+    let result = text.split("-36cd479b6b5-")
+    return result
 }
 
-function encrypt_params(url, body){
+function encrypt_params(url, body) {
     url = parse(url)
     let text = JSON.stringify(body)
     let message = `nobody${url.path}use${text}md5forencrypt`
@@ -35,4 +35,4 @@ function encrypt_params(url, body){
 }
 
 
-module.exports={decrypt_params,encrypt_params,decrypt,encrypt}
+module.exports = { decrypt_params, encrypt_params, decrypt, encrypt }
